247f79b11550dc1ce7fb4be084d01b40ec783e2f
Eric Flumerfelt <eflumerf@fnal.gov>
2020-12-18 11:00:30 -0600

    Add Tests to artdaq_mfextensions

---
+++ test/Extensions/throttle_t.cc
@@ -0,0 +1,32 @@
+#include "mfextensions/Extensions/throttle.hh"
+
+#define BOOST_TEST_MODULE throttle_t
+#include "cetlib/quiet_unit_test.hpp"
+#include "cetlib_except/exception.h"
+
+#define TRACE_NAME "throttle_t"
+#include "tracemf.h"
+
+BOOST_AUTO_TEST_SUITE(throttle_t)
+
+BOOST_AUTO_TEST_CASE(Throttle)
+{
+	throttle t("test", 2, 1);
+
+	struct timeval tv;
+	gettimeofday(&tv, nullptr);
+
+	BOOST_REQUIRE(!t.reach_limit("test", tv));
+	BOOST_REQUIRE(!t.reach_limit("test", tv));
+	BOOST_REQUIRE(t.reach_limit("test", tv));
+	BOOST_REQUIRE(!t.reach_limit("quiz", tv));
+
+	t.use(false);
+	BOOST_REQUIRE(!t.reach_limit("test", tv));
+
+	t.use(true);
+	tv.tv_sec += 2;
+	BOOST_REQUIRE(!t.reach_limit("test", tv));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
